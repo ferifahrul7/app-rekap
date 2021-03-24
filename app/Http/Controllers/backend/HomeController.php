2bd28992c54b9909e2670ad50ec9d5a53933e141
Feri Fahrul <ferifahrul1@gmail.com>
2021-03-24 08:00:42 +0700

    first commit

---
+++ app/Http/Controllers/backend/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\backend;
+
+use App\Http\Controllers\backend\BackendController as Controller;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $bcrum = $this->bcrum('Home');
+        return view('backend.home', compact('bcrum'));
+    }
+}
